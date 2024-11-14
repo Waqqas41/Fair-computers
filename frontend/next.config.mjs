c94af1e2d382abafb4d4ded0e89327ba2c2e8bfd
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2024-11-14 15:51:12 -0500

    fixed base path, prettier

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/Fairway-computers-Demo",
+  // basePath: "/Fairway-computers-Demo",
   images: {
     unoptimized: true,
   },
